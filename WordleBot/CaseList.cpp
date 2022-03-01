add6a384c35664cb5de70614d50830f345c3fe16
PaulBailey-1 <you@example.com>
2022-03-01 13:44:32 -0600

    Fixed file errors

--- WordleBot/CaseList.cpp
+++ WordleBot/CaseList.cpp
@@ -5,17 +5,26 @@
 Case CaseList::caseList[WORD_COUNT];
 
 void CaseList::load(std::string fileName) {
+
     std::ifstream caseListFileIn(fileName);
 
     if(!caseListFileIn.good()) {
+
         std::cout << "Creating Cases...\n";
-        std::ofstream caseListFile(fileName);
+
+		caseListFileIn.close();
+        std::ofstream caseListFileOut(fileName);
+
 		for (int i = 0; i < WordList::length; i++) {
+
 			std::string word = WordList::wordList[i];
-            caseListFile << "-\n" << word << "\n";
+            caseListFileOut << "-\n" << word << "\n";
+
 			for (int formIdx = 0; formIdx < FORM_COUNT; formIdx++) {
+
 				Form form = FormList::formList[formIdx];
 				bool valid = true;
+
 				switch (form.state) {
 					case State::PRESENT:
 						if (word[form.place] != form.letter) {
@@ -45,28 +54,36 @@ void CaseList::load(std::string fileName) {
 						break;
 				}
 				if (valid) {
-					caseListFile << formIdx << "\n";
+					caseListFileOut << formIdx << "\n";
 				}
 			}
         }
-        caseListFile.close();
+        caseListFileOut.close();
+		caseListFileIn.open(fileName);
     }
 
 	std::cout << "Loading Cases...\n";
+
 	std::string line;
 	int caseIdx = 0;
+
 	std::getline(caseListFileIn, line);
+
 	while (std::getline(caseListFileIn, line)) {
+
 		Case aCase;
 		aCase.word = line;
 		int formIdx = 0;
+
 		while (std::getline(caseListFileIn, line) && line != "-") {
 			aCase.validForms[formIdx] = stoi(line);
 			formIdx++;
 		}
+
 		caseList[caseIdx] = aCase;
 		caseIdx++;
 	}
+
 	caseIdx < WordList::length ? throw std::runtime_error("Case data invalid") : NULL;
 
     caseListFileIn.close();
--- WordleBot/FormList.cpp
+++ WordleBot/FormList.cpp
@@ -3,26 +3,33 @@
 Form FormList::formList[FORM_COUNT];
 
 void FormList::load(std::string fileName) {
+
     std::ifstream formListFileIn(fileName);
+
     if (!formListFileIn.good()) {
-        formListFileIn.close();
+
         std::cout << "Creating forms...\n";
-        std::ofstream formListFile(fileName);
+        formListFileIn.close();
+        std::ofstream formListFileOut(fileName);
+
         for (int letter = 0; letter < 26; letter++) {
             for (int place = 0; place < 5; place++) {
                 for (int state = 0; state < 3; state++) {
-                    formListFile << (char)(letter + 'a') << " " << place << " " << state << "\n";
+                    formListFileOut << (char)(letter + 'a') << " " << place << " " << state << "\n";
                 }
             }
         }
-        formListFile.close();
+
+        formListFileOut.close();
+        formListFileIn.open(fileName);
     }
 
     std::cout << "Loading forms...\n";
-    std::ifstream formListFile(fileName);
     std::string line;
     int i = 0;
+
     while (std::getline(formListFileIn, line)) {
+
         std::stringstream ss(line);
         ss >> formList[i].letter;
         ss >> formList[i].place;
@@ -31,8 +38,10 @@ void FormList::load(std::string fileName) {
         formList[i].state = (State)a;
         i++;
     }
+
     i < FORM_COUNT ? throw std::runtime_error("Form data invalid") : NULL;
 
-    formListFile.close();
+    formListFileIn.close();
+
     std::cout << "Done\n";
 }
